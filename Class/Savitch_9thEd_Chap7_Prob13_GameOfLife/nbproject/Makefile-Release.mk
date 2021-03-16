1d008c270894339639f3ad8bae03a7c9b38c5f8d
Mark Lehr <mark.lehr@rcc.edu>
2021-03-16 07:58:54 -0700

    Signed-off-by: Mark Lehr <mark.lehr@rcc.edu>

--- Class/Savitch_9thEd_Chap7_Prob13_GameOfLife/nbproject/Makefile-Release.mk
+++ Class/Savitch_9thEd_Chap7_Prob13_GameOfLife/nbproject/Makefile-Release.mk
@@ -62,7 +62,7 @@ ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/savitch_9thed_chap7_prob13_gameoflife
 	${MKDIR} -p ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}
 	${LINK.cc} -o ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/savitch_9thed_chap7_prob13_gameoflife ${OBJECTFILES} ${LDLIBSOPTIONS}
 
-${OBJECTDIR}/main.o: main.cpp 
+${OBJECTDIR}/main.o: main.cpp
 	${MKDIR} -p ${OBJECTDIR}
 	${RM} "$@.d"
 	$(COMPILE.cc) -O2 -std=c++11 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/main.o main.cpp
@@ -73,7 +73,6 @@ ${OBJECTDIR}/main.o: main.cpp
 # Clean Targets
 .clean-conf: ${CLEAN_SUBPROJECTS}
 	${RM} -r ${CND_BUILDDIR}/${CND_CONF}
-	${RM} ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/savitch_9thed_chap7_prob13_gameoflife.exe
 
 # Subprojects
 .clean-subprojects:
